7c0a9858c53a4c6d6f66c0c8d95fb4a71b7df0d3
Samuel Gomes <SamPongPie@gmail.com>
2025-01-08 20:36:30 -0300

    bug: loading error

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Display from "./assets/components/display"; 
+
 import { GlobalStyle } from "./assets/global/style"; 
-import Loading from "./assets/components/pages/loading"; 
-
 function App() {
-  const [loading, setLoading] = useState(true); 
-  const [data, setData] = useState(null); 
-
-  useEffect(() => {
-    setTimeout(() => {
-      setData({
-        home: {
-          logo: "",
-
-        }
-      });
-
-      setLoading(false); 
-    }, 1500); 
-  }, []);
-
-  if (loading) return <Loading />;
-  const { home } = data;
 
   return (
     <>
       <GlobalStyle />
-      <Display data={home} /> 
     </>
   );
 }
