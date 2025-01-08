5c624c9524321399f1fe917f7e0d054bc4a56075
Samuel Gomes <SamPongPie@gmail.com>
2025-01-08 20:42:19 -0300

    bug: css bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
+import Display from "./assets/components/display";
 import { GlobalStyle } from "./assets/global/style"; 
 function App() {
 
   return (
     <>
+      <Display/>
       <GlobalStyle />
     </>
   );
